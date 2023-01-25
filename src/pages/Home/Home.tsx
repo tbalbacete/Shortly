54251d57ac13534775760460d26f74f984c40290
Anthony.Albacete <Anthony.Albacete@rjet.com>
2023-01-25 18:31:14 -0500

    got working API call for short link

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,27 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 
 export const Home: React.FC = () => {
+    const [shortUrl, setShortUrl] = React.useState("test")
+    async function postData(url = "https://gotiny.cc/api	") {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({input: "https://www.youtube.com"})
+
+        });
+        return response.json();
+    }
+
+    const retrieveData =  () => {
+        return postData().then((data) => setShortUrl(`gotiny.cc/${data[0].code}`))
+    }
+
+    postData().then((data) => {console.log(data)})
     return (
     <div>
         <h1>Home</h1>
-        <Button variant="outlined">Test</Button>
+        <p>{shortUrl}</p>
+        <Button variant="outlined" onClick={() => retrieveData()}>Submit</Button>
     </div>
     )
 }
