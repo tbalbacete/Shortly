7ff46cd3e81d011634721436347e1a3ec0a89b09
Anthony.Albacete <Anthony.Albacete@rjet.com>
2023-01-26 14:44:43 -0500

    style updates for mobile, added img

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -2,20 +2,26 @@ import styled from "@emotion/styled";
 import { Button, TextField } from "@mui/material";
 
 export const StyledDiv = styled("div")({
+    "@media (max-width: 512px)": {
+        left: "9%"
+    },
     position: "fixed",
-    top: "40%",
+    top: "30%",
     left: "41%",
-    textAlign: "center"
+    textAlign: "center",
+})
+
+export const StyledImage = styled("img")({
+    height: "100px",
 })
 
 export const StyledTextField = styled(TextField)({
     marginLeft: "30px",
     background: "white",
-    // height: "2rem"
 })
 
 export const StyledButton = styled(Button)({
-    marginTop: "6px",
-    marginLeft: "10px",
-    // height: "2rem"
+    height: "48px",
+    borderLeft: "0px",
+    borderRadius: "0px 15px 15px 0px"
 })
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,47 @@
 import * as React from "react";
 import { postLinkData } from "../../utils";
 import { Footer } from "./Footer";
-import { StyledButton, StyledDiv, StyledTextField } from "./Home.styles";
+import {
+  StyledButton,
+  StyledDiv,
+  StyledImage,
+  StyledTextField,
+} from "./Home.styles";
 
 export const Home: React.FC = () => {
-    const [shortUrl, setShortUrl] = React.useState("")
-    const [isLoading, setIsloading] = React.useState(false);
+  const [shortUrl, setShortUrl] = React.useState("");
+  const [isLoading, setIsloading] = React.useState(false);
 
-    const handleSubmit =  (event: any) => {
-        event.preventDefault();
-        setIsloading(true);
-        return postLinkData(event.target.linkSubmit.value).then(function(data) { setIsloading(false); setShortUrl(`gotiny.cc/${data[0].code}`)})
-    };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setIsloading(true);
+    return postLinkData(event.target.linkSubmit.value).then(function (data) {
+      setIsloading(false);
+      setShortUrl(`gotiny.cc/${data[0].code}`);
+    });
+  };
 
-    return (
+  return (
     <div>
-        <StyledDiv>
+      <StyledDiv>
+        <StyledImage src="\src\assets\letter-s.png" alt="S-Logo" />
         <h1>Short-ly</h1>
         <form onSubmit={handleSubmit}>
-        <StyledTextField id="linkSubmit" label={"Enter link"} maxRows={1} size="small" variant="filled"></StyledTextField>
-        <StyledButton variant="outlined" type="submit">Submit</StyledButton>
+          <StyledTextField
+            id="linkSubmit"
+            label={"Enter link to shorten..."}
+            maxRows={1}
+            size="small"
+            variant="filled"
+          ></StyledTextField>
+          <StyledButton variant="outlined" type="submit">
+            Submit
+          </StyledButton>
         </form>
         <p>{isLoading ? "Loading..." : shortUrl}</p>
-        </StyledDiv>
-        <Footer />
+      </StyledDiv>
+      <Footer />
     </div>
-    )
-}
+  );
+};
